ea6b61d5839f34b3ce0c97146eb47e3f0c6585de
Eric L. Hepperle <codewizard13@gmail.com>
2024-01-03 15:35:35 -0600

    UPDATE wp/ SHORTCODE IphoneMsg:  ADD styles

--- snippets/wp/ehCodeSnip__WP__Shortcode__IphoneMsg.php
+++ snippets/wp/ehCodeSnip__WP__Shortcode__IphoneMsg.php
@@ -11,6 +11,7 @@ DESCRIPTION:
 
 - From tutorial: https://www.youtube.com/watch?v=pbekOEr0Wo0
 - Renders messages styled like iphone text messages via enclosing shortcodes
+- #GOTCHA: Triangle pointer had gap so tweaked bottom, right, left values for :before rules
 
 SAMPLE RESULTS:
 
@@ -48,4 +49,59 @@ function imsg_shortcode( $atts , $content = null ) {
 }
 add_shortcode( 'imsg', 'imsg_shortcode' );
 
+?>
+<style>
+/**
+SHORTCODE: imsg - iPhone text message style
+*/
 
+.imsg {
+  position: relative;
+  font-size: 1.3em;
+  font-family: "Helvetica Neue", Arial, sans-serif;
+  line-height: 1.3em;
+  -webkit-border-radius: 25px;
+  -moz-border-radius: 25px;
+  border-radius: 25px;
+  padding: 15px;
+}
+
+.imsg:before {
+  content: "";
+  display: block;
+  width: 0;
+  height: 0;
+  position: absolute;
+}
+
+.imsg.first {
+  background: #2C7CFE;
+  color: #fff;
+  margin: 20px 12px 20px 50px;
+}
+
+.imsg.first:before {
+  border-top: 12px solid transparent;
+  border-bottom: 12px solid transparent;
+  border-left: 12px solid #2C7CFE;
+  bottom: 19px;
+  right: -9px;
+}
+
+.imsg.reply {
+  background: #DDD;
+  color: #222;
+  margin: 20px 50px 20px 12px;
+}
+
+.imsg.reply:before {
+  border-top: 12px solid transparent;
+  border-bottom: 12px solid transparent;
+  border-right: 12px solid #DDD;
+  border-left: none;
+  bottom: 19px;
+  left: -9px;
+}
+
+</style>
+<?php
