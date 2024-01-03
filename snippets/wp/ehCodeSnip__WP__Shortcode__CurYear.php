94782a37498ed78912ee8e4bbf993808cb308dee
Eric L. Hepperle <codewizard13@gmail.com>
2024-01-03 12:09:57 -0600

    ADD wp/ Shortcode Current Year

---
+++ snippets/wp/ehCodeSnip__WP__Shortcode__CurYear.php
@@ -0,0 +1,45 @@
+<?php
+
+/* 
+Type:         WordPress Code Snippet
+Sub-Type:     SHORTCODE
+TITLE:        Kinsta: SHORTCODE - Current Year
+Author:       Eric Hepperle
+Date Created: 2024-01-03
+
+DESCRIPTION:
+
+- From tutorial: https://www.youtube.com/watch?v=izIDOiQgWcU
+
+- A demo of parsing parameters from shortcodes from developers.wordpress.org
+
+SAMPLE RESULTS:
+
+N/A
+
+SNIPPETS SETTINGS:
+- Run snippet everywhere
+
+TAGS:
+
+Shortcode, WordPress Developers, Shortcode Parameters, Shortcode Parsing
+
+REFERENCES:
+- N/A
+
+*/
+
+
+/**
+ * [current_year] returns the current Year as a 4-digit string.
+ * @return string Current Year
+ */
+
+add_shortcode( 'current_year', 'salcodes_year' );
+
+function salcodes_init() {
+	function salcodes_year() {
+		return getdate()['year'];
+	}
+}
+add_action( 'init', 'salcodes_init' );
