6a153c20989c1edaf2170f02581e77ea10a3b8ff
Eric L. Hepperle <codewizard13@gmail.com>
2024-01-03 13:11:44 -0600

    UPDATE wp/ SHORTCODE CustomizableCTABtn:  CREATE background color attribute

--- snippets/wp/ehCodeSnip__WP__Shortcode__CustomizableCTABtn.php
+++ snippets/wp/ehCodeSnip__WP__Shortcode__CustomizableCTABtn.php
@@ -1,16 +1,18 @@
 <?php
 
 /* 
-Type:         WordPress Code Snippet
-Sub-Type:     SHORTCODE
-TITLE:        Kinsta: SHORTCODE - Customizable CTA button
-Author:       Eric Hepperle
-Date Created: 2024-01-03
+Type:         	WordPress Code Snippet
+Sub-Type:     	SHORTCODE
+TITLE:        	Kinsta: SHORTCODE - Customizable CTA button
+Author:       	Eric Hepperle
+Date Created: 	2024-01-03
 
 DESCRIPTION:
 
 - From tutorial: https://www.youtube.com/watch?v=izIDOiQgWcU
 - Renders CTA button on front end with CSS style values passed as shortcode parameters
+- #GOTCHA: Change code from class="button ' . esc_attr( $a['color'] ) 
+to class="button " style="background:' . esc_attr( $a['color'] ). If we had enqueued a stylesheet, presumably there would be classes in there. But, since we are not enqueuing a stylesheet with named color classes, I edited the shortcode to add a style attribute to our button and set the background color that way.
 
 SAMPLE RESULTS:
 
@@ -45,18 +47,17 @@ function salcodes_cta( $atts ) {
  'label' => 'Button',
  'target' => '_self'
  ), $atts );
- $output = '<p><a href="' . esc_url( $a['link'] ) . '" id="' . esc_attr( $a['id'] ) . '" class="button ' . esc_attr( $a['color'] ) . ' ' . esc_attr( $a['size'] ) . '" target="' . esc_attr($a['target']) . '">' . esc_attr( $a['label'] ) . '</a></p>';
+ $output = '<p><a href="' . esc_url( $a['link'] ) . '" id="' . esc_attr( $a['id'] ) . '" class="button " style="background:' . esc_attr( $a['color'] ) . '" ' . esc_attr( $a['size'] ) . '" target="' . esc_attr($a['target']) . '">' . esc_attr( $a['label'] ) . '</a></p>';
  return $output;
 }
 
-
 /** Enqueuing the Stylesheet for the CTA Button */
 
-function salcodes_enqueue_scripts() {
-	global $post;
-	if( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'cta_button') ) {
-	wp_register_style( 'salcodes-stylesheet',  plugin_dir_url( __FILE__ ) . 'css/style.css' );
-			wp_enqueue_style( 'salcodes-stylesheet' );
-	}
- }
- add_action( 'wp_enqueue_scripts', 'salcodes_enqueue_scripts');
+// function salcodes_enqueue_scripts() {
+//  global $post;
+//  if( is_a( $post, 'WP_Post' ) && has_shortcode( $post->post_content, 'cta_button') ) {
+//  wp_register_style( 'salcodes-stylesheet',  plugin_dir_url( __FILE__ ) . 'css/style.css' );
+//      wp_enqueue_style( 'salcodes-stylesheet' );
+//  }
+// }
+// add_action( 'wp_enqueue_scripts', 'salcodes_enqueue_scripts');
