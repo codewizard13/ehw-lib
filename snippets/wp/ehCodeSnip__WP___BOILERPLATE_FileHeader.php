31a453a84561ce1d79805109cfc1ecacd9d953fb
Eric L. Hepperle <codewizard13@gmail.com>
2024-03-25 08:45:37 -0500

    ADD wp/  BOILERPLATE / STARTER header comment

---
+++ snippets/wp/ehCodeSnip__WP___BOILERPLATE_FileHeader.php
@@ -0,0 +1,41 @@
+<?php
+
+/* 
+Type:         	WordPress Code Snippet
+Sub-Type:     	N/A
+TITLE:        	[The Title You Might Put in Code Snippets]
+Author:       	Eric Hepperle
+Date Created: 	YYYY-MM-DD
+
+DESCRIPTION:
+
+- USE this file as a boilerplate starter file header comment.
+
+SAMPLE RESULTS:
+
+N/A
+
+USAGE:
+
+N/A
+
+SNIPPETS SETTINGS:
+- Only run in admin area
+
+REQUIREMENTS & DEPENDENCIES:
+- 
+
+TAGS:
+
+CPT, Custom Fields, Meta Fields, Calculations, Relationship Fields
+
+REFERENCES:
+- 
+
+*/
+
+
+/**
+ * CODE GOES HERE
+ */
+echo "Replace this code with your own!";
